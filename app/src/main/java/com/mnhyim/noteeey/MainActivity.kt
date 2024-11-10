720f04cf3fa01f2eb17ca4e5f69aaaabfd07e9a9
Muhamad Nur Hasyim <mnhyim76@gmail.com>
2024-11-10 21:20:43 +0800

    feat: Setup Hilt on project

--- app/src/main/java/com/mnhyim/noteeey/MainActivity.kt
+++ app/src/main/java/com/mnhyim/noteeey/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.mnhyim.noteeey.navigation.MainNavHost
 import com.mnhyim.noteeey.ui.theme.NoteeeyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
