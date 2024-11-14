289e1a74c0d1d9d005dc580ffc89ac38e005552b
Muhamad Nur Hasyim <mnhyim76@gmail.com>
2024-11-14 12:08:40 +0800

    feat: Developed custom dropdown

--- app/src/main/java/com/mnhyim/noteeey/ui/feature/addnote/AddNoteScreen.kt
+++ app/src/main/java/com/mnhyim/noteeey/ui/feature/addnote/AddNoteScreen.kt
@@ -1,30 +1,25 @@
 package com.mnhyim.noteeey.ui.feature.addnote
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.consumeWindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.mnhyim.noteeey.domain.model.Category
 import com.mnhyim.noteeey.navigation.Routes
 import com.mohamedrejeb.richeditor.model.rememberRichTextState
-import com.mohamedrejeb.richeditor.ui.material3.RichTextEditor
-import com.mohamedrejeb.richeditor.ui.material3.RichTextEditorDefaults
+import java.time.LocalDateTime
 
 @Composable
 fun AddNoteScreen(
@@ -46,7 +41,6 @@ fun AddNoteScreen(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun AddNoteScreenContent(
     modifier: Modifier = Modifier
@@ -54,41 +48,79 @@ private fun AddNoteScreenContent(
     val richTextState = rememberRichTextState()
     var title by remember { mutableStateOf("") }
 
+    /* TODO: Temporary stuff */
+    val categories = (0..19).map { index ->
+        Category(
+            id = index.toLong(),
+            name = "Item ${index + 1}",
+            createdAt = LocalDateTime.now(),
+            updatedAt = LocalDateTime.now()
+        )
+    }
+    var selectedCategoryId by remember { mutableLongStateOf(-1) }
+
     Column(
         modifier = modifier.padding(16.dp)
     ) {
-        TextField(
-            value = title,
-            onValueChange = { title = it },
-            colors = TextFieldDefaults.colors(
-                focusedContainerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
-                unfocusedContainerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
-                unfocusedIndicatorColor = Color.Transparent,
-                focusedIndicatorColor = Color.Transparent
-            ),
-            shape = MaterialTheme.shapes.medium,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 8.dp)
+        CustomDropdown(
+            selectedItemName = if (selectedCategoryId == -1L) "Category" else categories[selectedCategoryId.toInt()].name,
+            list = { closeDialog ->
+                LazyColumn {
+                    items(items = categories, key = { it.id }) { category ->
+                        CustomDropdownMenuItem(
+                            text = category.name,
+                            isSelected = selectedCategoryId == category.id,
+                            onClick = {
+                                selectedCategoryId = category.id
+                                closeDialog()
+                            }
+                        )
+                    }
+                }
+            },
+            modifier = Modifier.fillMaxWidth(0.5f)
         )
-        Box {
-            RichTextEditor(
-                state = richTextState,
-                colors = RichTextEditorDefaults.richTextEditorColors(
-                    containerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
-                    unfocusedIndicatorColor = Color.Transparent,
-                    focusedIndicatorColor = Color.Transparent
-                ),
-                shape = MaterialTheme.shapes.medium,
-                modifier = Modifier.fillMaxSize(),
-                contentPadding = RichTextEditorDefaults.richTextEditorWithLabelPadding(bottom = 74.dp)
-            )
-            RtfStyleBar(
-                state = richTextState,
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(4.dp)
-            )
-        }
+//        Row(
+//            verticalAlignment = Alignment.CenterVertically,
+//            modifier = Modifier.padding(bottom = 8.dp)
+//        ) {
+//            TextButton(
+//                onClick = {},
+//                modifier = Modifier.weight(0.3f)
+//            ) {
+//                Text("A")
+//            }
+//            TextField(
+//                value = title,
+//                onValueChange = { title = it },
+//                colors = TextFieldDefaults.colors(
+//                    focusedContainerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
+//                    unfocusedContainerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
+//                    unfocusedIndicatorColor = Color.Transparent,
+//                    focusedIndicatorColor = Color.Transparent
+//                ),
+//                shape = MaterialTheme.shapes.medium,
+//                modifier = Modifier.weight(0.7f)
+//            )
+//        }
+//        Box {
+//            RichTextEditor(
+//                state = richTextState,
+//                colors = RichTextEditorDefaults.richTextEditorColors(
+//                    containerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
+//                    unfocusedIndicatorColor = Color.Transparent,
+//                    focusedIndicatorColor = Color.Transparent
+//                ),
+//                shape = MaterialTheme.shapes.medium,
+//                modifier = Modifier.fillMaxSize(),
+//                contentPadding = RichTextEditorDefaults.richTextEditorWithLabelPadding(bottom = 74.dp)
+//            )
+//            RtfStyleBar(
+//                state = richTextState,
+//                modifier = Modifier
+//                    .align(Alignment.BottomCenter)
+//                    .padding(4.dp)
+//            )
+//        }
     }
 }
---
+++ app/src/main/java/com/mnhyim/noteeey/ui/feature/addnote/CustomDropdown.kt
@@ -0,0 +1,98 @@
+package com.mnhyim.noteeey.ui.feature.addnote
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
+import androidx.compose.material3.Card
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+
+@Composable
+fun CustomDropdown(
+    selectedItemName: String,
+    list: @Composable (() -> Unit) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    if (expanded) {
+        Dialog(
+            onDismissRequest = { expanded = false },
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth(0.95f)
+                    .heightIn(max = (LocalConfiguration.current.screenHeightDp * 0.65f).dp)
+            ) {
+                list { expanded = false }
+            }
+        }
+    }
+
+    Box(
+        modifier = modifier.height(IntrinsicSize.Min)
+    ) {
+        OutlinedTextField(
+            value = selectedItemName,
+            onValueChange = { },
+            label = { Text("Label") },
+            trailingIcon = {
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.ArrowDropUp else Icons.Filled.ArrowDropDown,
+                    contentDescription = "",
+                    modifier = Modifier.clickable { expanded = true },
+                )
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 8.dp)
+                .clip(MaterialTheme.shapes.extraSmall)
+                .clickable { expanded = true },
+        )
+    }
+}
+
+@Composable
+fun CustomDropdownMenuItem(
+    text: String,
+    onClick: () -> Unit,
+    isSelected: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        color = if (isSelected) MaterialTheme.colorScheme.primary else Color.Unspecified,
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable { onClick() }
+            .padding(16.dp)
+    )
+    HorizontalDivider(
+        modifier = Modifier
+    )
+}
